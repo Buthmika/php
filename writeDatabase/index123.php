41f16be0cb13367880d8faf9584710fa878c3ad3
Buthmika <buthmikamenushan@gmail.com>
2025-04-17 16:15:09 +0530

    Create index123.php

---
+++ writeDatabase/index123.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+</head>
+<body>
+      <form action=""metod='get'>
+            First Name<input type="text" name="firstName" placeholder="First Name"><br>
+            Last Name<input type="text" name="lastName" placeholder="Last Name"><br>
+            Email<input type="email" name="email" placeholder="Email"><br>
+      </form>
+</body>
+</html>
