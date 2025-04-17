cc71a79393c7f8531f20d4aa5b50c8ad77d12a47
Buthmika <buthmikamenushan@gmail.com>
2025-04-17 09:51:13 +0530

    create db connection

--- db.php
+++ db.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Document</title>
-</head>
-<body>
-       
-</body>
-</html>
+<?php 
+$dbServerName='localhost';
+$dbUserName='root';
+$dbPassword='';
+$dbName='phptutorials';
+$connect=mysqli_connect($dbServerName,$dbUserName,$dbPassword,$dbName);
+
+?>
