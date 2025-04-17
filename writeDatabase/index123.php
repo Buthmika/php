d32c7931a8cd50f9ac28a6776e873bc3cdcf73e5
Buthmika <buthmikamenushan@gmail.com>
2025-04-17 16:48:02 +0530

    Update index123.php

--- writeDatabase/index123.php
+++ writeDatabase/index123.php
@@ -4,12 +4,13 @@
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Document</title>
+      <link rel="stylesheet" href="index123.css">
 </head>
 <body>
       <form action="register.php" method='post'>
-            First Name<input type="text" name="firstName" placeholder="First Name"><br>
-            Last Name<input type="text" name="lastName" placeholder="Last Name"><br>
-            Email<input type="email" name="email" placeholder="Email"><br>
+            <label for="">First Name</label><input type="text" name="firstName" placeholder="First Name"><br>
+            <label for="">Last Name</label><input type="text" name="lastName" placeholder="Last Name"><br>
+            <label for="">Email</label><input type="email" name="email" placeholder="Email"><br>
             <button type='submit' name='Register'>Register</button>
       </form>
 </body>
