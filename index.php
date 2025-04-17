f7775c266c3f24b596793e984fb163b01590f9da
Buthmika <buthmikamenushan@gmail.com>
2025-04-17 10:04:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,15 @@ include_once 'db.php';
       <title>Document</title>
 </head>
 <body>
-      
+      <?php
+      $sql="SELECT * FROM users";
+      $result=mysqli_query($connect,$sql);
+      $checkResult=mysqli_num_rows($result);
+      if($checkResult>0){
+            while($row=mysqli_fetch_assoc($result)){
+                  echo $row['email']."<br>";
+            }
+      }  
+      ?>
 </body>
 </html>
