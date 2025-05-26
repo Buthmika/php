84a9ba9b7c45142a2b013cc98d40d940ce503bf0
Buthmika <buthmikamenushan@gmail.com>
2025-05-26 20:02:46 +0530

    Update Create.tsx

--- laravelReactCrud/resources/js/pages/Products/Create.tsx
+++ laravelReactCrud/resources/js/pages/Products/Create.tsx
@@ -2,6 +2,8 @@ import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head,Link } from '@inertiajs/react';
 import { Button } from '@/components/ui/button';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -13,9 +15,14 @@ const breadcrumbs: BreadcrumbItem[] = [
 export default function Index() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
-            <Head title="products" />
-            <div className='m-4'>
-                
+            <Head title="Create New Product" />
+            <div className='w-8/12 p-4'>
+                <form action="">
+                    <div>
+                        <Label htmlFor="Product Name">Name</Label>
+                        <Input placeholder='Product Name'></Input>
+                    </div>
+                </form>
             </div>
         </AppLayout>
     );
