ef1624be0964475717c86853b9bd5619b1101c47
Buthmika <buthmikamenushan@gmail.com>
2025-05-26 19:52:15 +0530

    Update web.php

--- laravelReactCrud/routes/web.php
+++ laravelReactCrud/routes/web.php
@@ -13,7 +13,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
     Route:: get('/products',[ProductController:: class,'index'])->name('products.index');
-    Route::get('/products/create',[ProductController:: class,'create'])->name('product.create');
+    Route::get('/products/create',[ProductController:: class,'create'])->name('products.create');
 });
 
 require __DIR__.'/settings.php';
