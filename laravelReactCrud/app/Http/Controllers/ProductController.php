f6079fae44961d48b08dec7730b637d7e037b76a
Buthmika <buthmikamenushan@gmail.com>
2025-05-27 11:36:04 +0530

    Update ProductController.php

--- laravelReactCrud/app/Http/Controllers/ProductController.php
+++ laravelReactCrud/app/Http/Controllers/ProductController.php
@@ -13,4 +13,8 @@ class ProductController extends Controller
     public function create(){
         return Inertia:: render('Products/Create',[]);
     }
+    public function store(Request $_request){
+        dd($_request);
+
+    }
 }
