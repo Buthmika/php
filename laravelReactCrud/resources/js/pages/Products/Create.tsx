fdc591ff20f6ccba0666a8145d6d8ec4c701a4a6
Buthmika <buthmikamenushan@gmail.com>
2025-05-27 11:25:46 +0530

    Update Create.tsx

--- laravelReactCrud/resources/js/pages/Products/Create.tsx
+++ laravelReactCrud/resources/js/pages/Products/Create.tsx
@@ -1,9 +1,10 @@
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
-import { Head,Link } from '@inertiajs/react';
+import { Head,Link, useForm } from '@inertiajs/react';
 import { Button } from '@/components/ui/button';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -13,19 +14,37 @@ const breadcrumbs: BreadcrumbItem[] = [
 ];
 
 export default function Index() {
+    const{data,setData,post,processing,error}=useForm({
+        name:'apple',
+        price:'',
+        descripton:''
+    });
+    const handleSubmit=(e:React.FormEvent)=>{
+        e.preventDefault();
+        console.log(data)
+    }
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Create New Product" />
             <div className="w-8/12 p-4">
-                <form action="">
+                <form action="" className="space-y-4" onSubmit={handleSubmit}>
                     <div className="gap-1.5">
                         <Label htmlFor="Product Name">Name</Label>
-                        <Input placeholder="Product Name"></Input>
+                        <Input placeholder="Product Name" value={data.name} onChange={(e) => setData('name', e.target.value)}></Input>
                     </div>
                     <div className="gap-1.5">
                         <Label htmlFor="Product Price">Price</Label>
-                        <Input placeholder="Product Price"></Input>
+                        <Input placeholder="Product Price" value={data.price} onChange={(e) => setData('price', e.target.value)}></Input>
                     </div>
+                    <div className="gap-1.5">
+                        <Label htmlFor="Product Price">Description</Label>
+                        <Textarea
+                            placeholder="Description"
+                            value={data.descripton}
+                            onChange={(e) => setData('descripton', e.target.value)}
+                        ></Textarea>
+                    </div>
+                    <Button type="Submit">Add Product</Button>
                 </form>
             </div>
         </AppLayout>
